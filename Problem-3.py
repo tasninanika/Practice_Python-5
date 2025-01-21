6ddd14f422b6cef57e8285e9b4b9f3107a57bc31
anikatasnin <jarintasnin09@gmail.com>
2025-01-21 14:34:45 +0600

    Added problem-3

--- Problem-3.py
+++ Problem-3.py
@@ -5,3 +5,24 @@ Created on Tue Jan 21 14:24:24 2025
 @author: Jarin
 """
 
+# Given two integer variables a and b, and a boolean variable flag. The task is to check the status and return accordingly.
+
+#Return True for the following cases:
+
+#Either a or b (not both) is non-negative and the flag is false.
+#Both a and b are negative and the flag is true.
+#Otherwise, return False.
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+
+if a > 0 and b < 0 or a < 0 and b > 0:
+    flag = "false"
+    print("true")
+elif a < 0 and b < 0:
+    flag = "true"
+    print("true")
+elif a > 0 and b > 0:
+    print("flase")
